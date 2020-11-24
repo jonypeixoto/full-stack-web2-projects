f98e08b56999a0c3240ad9106ab469b39d6d05dc
JonyPeixoto <jonathanppeixoto@gmail.com>
2020-11-23 23:35:59 -0300

    update front-end-projects

---
+++ Front-End-Projects/Project04-Facebook-Page-Login-Screen/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Facebook Login Sign up created in 2020</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+		<!-- login Page start -->
+		<div class="loginsignup">
+			<div class="container">
+				<div class="row justify-content-between">
+					<div class="content-left">
+						<h1>facebook</h1>
+						<h2>Facebook helps you connect and share with the people in your life.</h2>
+					</div>
+					<div class="content-right">
+						<form action="">
+							<div class="form-group">
+								<input type="text" placeholder="Email address or phone number">
+							</div>
+							<div class="form-group">
+								<input type="password" placeholder="Password">
+							</div>
+							<div class="login">
+								<a href="" class="btn">log in</a>
+							</div>
+							<div class="forgot">
+								<a href="">Forgotten account?</a>
+							</div>
+							<div class="create-btn">
+								<a href="" class="btn">create new account</a>
+							</div>
+						</form>
+						<p><a href="">Create a Page</a> for a celebrity, band or business.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- login Page end -->
+</body>
+</html>
