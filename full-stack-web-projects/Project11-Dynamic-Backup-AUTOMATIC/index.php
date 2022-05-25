c3eaef7cb6cb1080f23d821655ee74b9c1f84579
JonyPeixoto <jonathanppeixoto@gmail.com>
2022-05-25 04:53:26 -0300

    updates

---
+++ full-stack-web-projects/Project11-Dynamic-Backup-AUTOMATIC/index.php
@@ -0,0 +1,51 @@
+<?php
+
+    // Defining local time
+    date_default_timezone_set('America/Sao_Paulo');
+    //
+
+    define('HOST','localhost');
+    define('DB','tests');
+    define('USER','root');
+    define('PASS','');
+
+    function backup(){
+        $db = new PDO('mysql:host='.HOST.';dbname='.DB,USER,PASS);
+
+        // Defining the file's name + date of the backup
+        $f = fopen(date('Y-m-d').'.txt','wt');
+        //
+
+        $tables = $db->query('SHOW TABLES');
+        foreach ($tables as $table) {
+            $sql = '-- TABLE: '.$table[0].PHP_EOL;
+            $create = $db->query('SHOW CREATE TABLE `'.$table[0].'`')->fetch();
+            $sql.=$create['Create Table'].';'.PHP_EOL;
+            fwrite($f, $sql);
+
+            $rows = $db->query('SELECT * FROM `'.$table[0].'`');
+			$rows->setFetchMode(PDO::FETCH_ASSOC);
+			foreach ($rows as $row) {
+				$row = array_map(array($db,'quote'),$row);
+				$sql = 'INSERT INTO `'.$table[0].'` (`'.implode('`, `',array_keys($row)).'`) VALUES ('.utf8_encode(implode(', ',$row)).');'. PHP_EOL;
+				fwrite($f, $sql);
+			}
+
+            $sql = PHP_EOL;
+            $result = fwrite($f, $sql);
+
+            if($result != false){
+                echo 'BACKUP SUCCESSFULLY MADE!';
+            }else{
+                echo 'OPS FAILED!';
+            }
+
+            flush();
+
+        }
+        fclose($f);
+    }
+
+    backup();
+
+?>
