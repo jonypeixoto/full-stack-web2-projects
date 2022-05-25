387b882e298a653eb1092780b82932561679df7e
JonyPeixoto <jonathanppeixoto@gmail.com>
2022-05-25 04:13:55 -0300

    UPDATES

---
+++ full-stack-web-projects/Project02-WebApp-Facebook-Clone-Flexbox-ReactJS-And-Firebase/src/Stories.js
@@ -0,0 +1,24 @@
+import cybertimeup_bg from './resources/cybertimeup_bg.jpg';
+
+export default function(){
+    return (
+        <div className="stories">
+            <div className="storiesCard" style={{backgroundImage:`url(${cybertimeup_bg})`}}>
+            <p>Jony Peixoto</p>
+            </div>
+            <div className="storiesCard" style={{backgroundImage:`url(${cybertimeup_bg})`}}>
+            <p>Jony Peixoto</p>
+            </div>
+            <div className="storiesCard" style={{backgroundImage:`url(${cybertimeup_bg})`}}>
+            <p>Jony Peixoto</p>
+            </div>
+            <div className="storiesCard" style={{backgroundImage:`url(${cybertimeup_bg})`}}>
+            <p>Jony Peixoto</p>
+            </div>
+            <div className="storiesCard" style={{backgroundImage:`url(${cybertimeup_bg})`}}>
+            <p>Jony Peixoto</p>
+        </div>
+      </div>
+    );
+}
+
