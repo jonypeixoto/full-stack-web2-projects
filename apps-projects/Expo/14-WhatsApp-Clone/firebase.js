2e707e2206c2a963b35e62c5553d06d74808dbe5
Jonathan P. Peixoto 喬尼·佩肖托 <00019805@educacao.cetiqt.senai.br>
2023-05-19 23:38:13 -0300

    🍏 Update README WhatsApp Clone App

---
+++ apps-projects/Expo/14-WhatsApp-Clone/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { initializeFirestore } from "firebase/firestore";
+const firebaseConfig = {
+ // your config goes here
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+});
+
+export function signIn(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+export function signUp(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
