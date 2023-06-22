9345950714318b77e94003f9b47a4f250a1e1253
Jonathan P. Peixoto 喬尼·佩肖托 <00019805@educacao.cetiqt.senai.br>
2023-06-21 22:56:43 -0300

    🕹️ Update README Project15-4

---
+++ full-stack-web-projects/Project15-4-Dynamic-Site-Advanced-and-Control-Panel-AdvancedPHP-and-News-Portal-and-Management/index.php
@@ -0,0 +1,133 @@
+<?php include('config.php'); ?>
+<?php Site::updateOnlineUser(); ?>
+<?php Site::counter(); ?>
+<?php 
+	$infoSite = MySql::connect()->prepare("SELECT * FROM `tb_site.config`");
+	$infoSite->execute();
+	$infoSite = $infoSite->fetch();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $infoSite['title']; ?></title>
+	<link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>style/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	<link href="<?php echo INCLUDE_PATH; ?>style/style.css" rel="stylesheet" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="keywords" content="keywords,of,my,website">
+	<meta name="description" content="Description of my website">
+	<link rel="icon" href="<?php echo INCLUDE_PATH; ?>favicon.ico" type="image/x-icon" />
+	<meta charset="utf-8" />
+</head>
+<body>
+<base base="<?php echo INCLUDE_PATH; ?>" />
+	<?php
+		$url = isset($_GET['url']) ? $_GET['url'] : 'home';
+		switch ($url) {
+			case 'testimonials':
+				echo '<target target="testimonials" />';
+				break;
+			
+			case 'services':
+				echo '<target target="services" />';
+				break;
+
+	
+		}
+	?>
+	<div class="success">Form sent sucessfully!</div>
+	<div class="overlay-loading">
+		<img src="<?php echo INCLUDE_PATH ?>images/ajax-loader.gif" />
+	</div><!--overlay-loading-->
+
+	<header>
+		<div class="center">
+			<div class="logo left"><a href="/">Logo</a></div><!--logo-->
+			<nav class="desktop right">
+				<ul>
+					<li><a href="<?php echo INCLUDE_PATH; ?>">Home</a></li>
+					<li><a href="<?php echo INCLUDE_PATH; ?>testimonials">Testimonials</a></li>
+					<li><a href="<?php echo INCLUDE_PATH; ?>services">Services</a></li>
+					<li><a href="<?php echo INCLUDE_PATH; ?>news">News</a></li>
+					<li><a realtime="contact" href="<?php echo INCLUDE_PATH; ?>contact">Contact</a></li>
+				</ul>
+			</nav>
+			 <nav class="mobile right">
+				<div class="button-menu-mobile">
+					<i class="fa fa-bars" aria-hidden="true"></i>
+				</div>
+				<ul>
+					<li><a href="<?php echo INCLUDE_PATH; ?>">Home</a></li>
+					<li><a href="<?php echo INCLUDE_PATH; ?>testimonials">Testimonials</a></li>
+					<li><a href="<?php echo INCLUDE_PATH; ?>services">Services</a></li>
+					<li><a href="<?php echo INCLUDE_PATH; ?>news">News</a></li>
+					<li><a realtime="contact" href="<?php echo INCLUDE_PATH; ?>contact">Contact</a></li>
+				</ul>
+			</nav>
+		<div class="clear"></div><!--clear-->
+		</div><!--center-->
+	</header>
+
+	<div class="container-principal">
+	<?php
+
+		if(file_exists('pages/'.$url.'.php')){
+			include('pages/'.$url.'.php');
+		}else{
+			// We can do something, because the page does not exist.
+			if($url != 'testimonials' && $url != 'services'){
+				$urlPar = explode('/',$url)[0];
+				if($urlPar != 'news'){
+				$page404 = true;
+				include('pages/404.php');
+				}else{
+					include('pages/news.php');
+				}
+			}else{
+				include('pages/home.php');
+			}
+		}
+
+	?>
+
+	</div><!--container-principal-->
+
+	<footer <?php if(isset($page404) && $page404 == true) echo 'class="fixed"'; ?>>
+		<div class="center">
+			<p>All rights reserved!</p>
+		</div>
+	</footer>
+	<script src="<?php echo INCLUDE_PATH; ?>js/jquery.js"></script>
+	<script src="<?php echo INCLUDE_PATH; ?>js/jquery.mask.js"></script>
+	<script src="<?php echo INCLUDE_PATH; ?>js/constants.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDHPNQxozOzQSZ-djvWGOBUsHkBUoT_qH4"></script>
+	<script src="<?php echo INCLUDE_PATH; ?>js/map.js"></script>
+	<script src="<?php echo INCLUDE_PATH; ?>js/scripts.js"></script>
+	<?php
+		if($url == 'home' || $url == ''){
+	?> 
+	<script src="<?php echo INCLUDE_PATH; ?>js/slider.js"></script>
+	<?php } ?>
+	<?php
+		if($url == 'contact'){
+	?> 
+	<?php } ?>
+
+	<?php
+		if(is_array($url) && strstr($url[0],'news') !== false){
+	?>
+		<script>
+			$(function(){
+				$('select').change(function(){
+					location.href=include_path+"news/"+$(this).val();
+				})
+			});
+		</script>
+	<?php 
+		}
+	?>
+
+	<script src="<?php echo INCLUDE_PATH; ?>js/exemple.js"></script>
+	<script src="<?php echo INCLUDE_PATH; ?>js/forms.js"></script>
+</body>
+</html>
