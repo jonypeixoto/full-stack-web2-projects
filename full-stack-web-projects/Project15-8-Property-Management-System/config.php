8437fe1a60dceea634b2e74b54c82517074c4d79
Jonathan P. Peixoto 喬尼·佩肖托 <info@jonypeixoto.com>
2023-08-02 19:04:39 -0300

    📚 Update README Project15-8

---
+++ full-stack-web-projects/Project15-8-Property-Management-System/config.php
@@ -0,0 +1,64 @@
+<?php
+session_start();
+ob_start();
+date_default_timezone_set('Europe/London');
+$autoload = function($class){
+    if($class == 'Email'){
+        require_once('classes/phpmailer/PHPMailerAutoload.php');
+    }
+    include('classes/'.$class.'.php');
+};
+
+spl_autoload_register($autoload);
+
+define('INCLUDE_PATH','https://localhost/full-stack-web2-projects/full-stack-projects/Project15-8-Property-Management-System/');
+define('INCLUDE_PATH_PANEL',INCLUDE_PATH.'panel/');
+
+define('BASE_DIR_PANEL',__DIR__.'/panel');
+
+
+//Connected with database!
+define('HOST','localhost');
+define('USER','root');
+define('PASSWORD','');
+define('DATABASE','system');
+
+//Contants to the control panel
+define('NAME_COMPANY','CybertimeUP');
+
+//Functions of the panel 
+function takeOffice($index){
+    return Panel::$offices[$index];
+}
+
+function selectedMenu($par){
+    /*<i class="fa fa-angle-double-right" aria-hidden="true"></i>*/
+    $url = explode('/',@$_GET['url'])[0];
+    if($url == $par){
+        echo 'class="menu-active"';
+    }
+}
+
+function verifyPermissionMenu($permission){
+    if($_SESSION['office'] >= $permission){
+        return;
+    }else{
+        echo 'style="display:none;"';
+    }
+}
+
+function verifyPermissionPage($permission){
+    if($_SESSION['office'] >= $permission){
+        return;
+    }else{
+        include('panel/pages/permission_denied.php');
+        die();
+    }
+}
+
+function recoverPost($post){
+    if(isset($_POST[$post])){
+        echo $_POST[$post];
+    }
+}
+?>
