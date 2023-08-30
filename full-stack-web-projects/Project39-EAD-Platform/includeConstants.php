e5fcbe3904b1618428e867cacf88e306c0d4c2e2
Jonathan P. Peixoto 喬尼·佩肖托 <info@jonypeixoto.com>
2023-08-30 12:44:53 -0300

    ☕ Update README Project39-EAD-Platform

---
+++ full-stack-web-projects/Project39-EAD-Platform/includeConstants.php
@@ -0,0 +1,27 @@
+<?php
+	session_start();
+	date_default_timezone_set('America/Sao_Paulo');
+	require('vendor/autoload.php');
+	$autoload = function($class){
+		if($class == 'Email'){
+			require_once('classes/phpmailer/PHPMailerAutoload.php');
+		}
+		include('classes/'.$class.'.php');
+	};
+
+	spl_autoload_register($autoload);
+
+
+	define('INCLUDE_PATH','http://localhost/ead2/');
+	define('INCLUDE_PATH_PAINEL',INCLUDE_PATH.'painel/');
+
+	define('BASE_DIR_PAINEL',__DIR__.'/painel');
+	//Conectar com banco de dados!
+	define('HOST','localhost');
+	define('USER','root');
+	define('PASSWORD','');
+	define('DATABASE','ead2');
+
+	//Constantes para o painel de controle
+	define('NOME_EMPRESA','CybertimeUP');
+?
